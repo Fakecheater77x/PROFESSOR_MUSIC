1d6d8631209b33a09370c323728e788d1f784ec2
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-11-30 09:18:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","PROFESSOR_XX7")
+OWNER_USERNAME = getenv("OWNER_USERNAME","PROFESSOR_opx")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME","PROFESSOR_MUSIC_BOT")
 # --------------------------------------------------------
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/poison_vibes_143")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ROYALS_TEAM_143")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PROFESSOR_UPDATES")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PROFESSOR_BOT_SUPPORT")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
